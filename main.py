870d152240bc70758b55983b35f966765050f131
Helen <kozovyakina@gmail.com>
2025-03-24 21:26:49 +0300

    change file

--- main.py
+++ main.py
@@ -3573,38 +3573,38 @@ from os import write
 #     f.seek(0)
 #     f.write(data)
 
-import os
-
-file = "text3.txt"
-# f = open(file, "w")
-# f.write("Замена строки в текстовом файле;\n"
-#         "изменить строку в списке;\n"
-#         "записать список в файл;\n")
-# f.close()
-
-if os.path.exists(file):
-    f = open(file, "r")
-    read_line = f.readlines()
-    f.close()
-
-    pos1 = int(input("pos1 = ")) - 1  # 1
-    pos2 = int(input("pos2 = ")) - 1  # 3
-
-    # if 0 <= pos1 < len(read_line) and 0 <= pos2 < len(read_line):
-    # if 0 < pos1 <= len(read_line) and 0 < pos2 <= len(read_line):
-    if 0 <= pos1 < len(read_line) and 0 <= pos2 < len(read_line):
-        read_line[pos1], read_line[pos2] = read_line[pos2], read_line[pos1]
-    else:
-        print("Такой строки нет")
-    print(read_line)
-
-    f = open(file, "w")
-    f.writelines(read_line)
-    f.close()
-
-else:
-    print("Такого файла нет")
+# import os
+#
+# file = "text3.txt"
+# # f = open(file, "w")
+# # f.write("Замена строки в текстовом файле;\n"
+# #         "изменить строку в списке;\n"
+# #         "записать список в файл;\n")
+# # f.close()
+#
+# if os.path.exists(file):
+#     f = open(file, "r")
+#     read_line = f.readlines()
+#     f.close()
+#
+#     pos1 = int(input("pos1 = ")) - 1  # 1
+#     pos2 = int(input("pos2 = ")) - 1  # 3
+#
+#     # if 0 <= pos1 < len(read_line) and 0 <= pos2 < len(read_line):
+#     # if 0 < pos1 <= len(read_line) and 0 < pos2 <= len(read_line):
+#     if 0 <= pos1 < len(read_line) and 0 <= pos2 < len(read_line):
+#         read_line[pos1], read_line[pos2] = read_line[pos2], read_line[pos1]
+#     else:
+#         print("Такой строки нет")
+#     print(read_line)
+#
+#     f = open(file, "w")
+#     f.writelines(read_line)
+#     f.close()
+#
+# else:
+#     print("Такого файла нет")
 # s = "Hello\"World"
 # print(len(s))
 
-
+print("Данные в локальном репозитории")
