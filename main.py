9f1ab3720c9e0706da9ce3743b607f21f41f0145
Helen <kozovyakina@gmail.com>
2025-05-19 21:30:31 +0300

    db

--- main.py
+++ main.py
@@ -6378,3 +6378,25 @@ import csv
 #     for d in data:
 #         writer.writerow(d)
 
+import sqlite3
+
+
+# con = sqlite3.connect("profile.db")
+# cur = con.cursor()
+#
+# cur.execute("""""")
+#
+# con.close()
+
+# with sqlite3.connect("profile.db") as con:
+#     cur = con.cursor()
+#     cur.execute("""CREATE TABLE IF NOT EXISTS users(
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     name TEXT NOT NULL,
+#     summa REAL,
+#     date BLOB
+#     )""")
+#     cur.execute("DROP TABLE users")
+
+
+
