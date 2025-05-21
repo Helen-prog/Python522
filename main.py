d2bf00ef73989c6b08d577c8985ce5fb94245617
Helen <kozovyakina@gmail.com>
2025-05-21 21:31:27 +0300

    db

--- main.py
+++ main.py
@@ -6378,7 +6378,7 @@ import csv
 #     for d in data:
 #         writer.writerow(d)
 
-import sqlite3
+# import sqlite3
 
 
 # con = sqlite3.connect("profile.db")
@@ -6397,6 +6397,65 @@ import sqlite3
 #     date BLOB
 #     )""")
 #     cur.execute("DROP TABLE users")
+
+#
+# import sqlite3
+
+# with sqlite3.connect("users.db") as con:
+#     cur = con.cursor()
+# cur.execute("""
+# CREATE TABLE IF NOT EXISTS person(
+# id INTEGER PRIMARY KEY AUTOINCREMENT,
+# name TEXT NOT NULL,
+# phone BLOB NOT NULL DEFAULT "+79090000000",
+# age INTEGER CHECK(age > 0 AND age < 100),
+# email TEXT UNIQUE
+# )
+# """)
+# cur.execute("""
+# ALTER TABLE person
+# RENAME TO person_table;
+# """)
+# cur.execute("""
+# ALTER TABLE person_table
+# ADD COLUMN address TEXT
+# """)
+# cur.execute("""
+# ALTER TABLE person_table
+# RENAME COLUMN address TO home_address
+# """)
+
+# cur.execute("""
+#    ALTER TABLE person_table
+#    DROP COLUMN home_address
+#    """)
+
+# cur.execute("""
+#    DROP TABLE person_table
+#    """)
+
+
+import sqlite3
+
+# with sqlite3.connect("db_3.db") as con:
+#     cur = con.cursor()
+#     cur.execute("""
+#     SELECT *
+#     FROM T1
+#     LIMIT 2, 5
+#     """)
+#
+#     # for res in cur:
+#     #     print(res)
+#
+#     # res = cur.fetchall()
+#     # print(res)
+#
+#     res2 = cur.fetchmany(2)
+#     print(res2)
+#
+#     res1 = cur.fetchone()
+#     print(res1)
 
 
 
