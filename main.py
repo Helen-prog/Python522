f77a25b771bbbdbb068f413bd850c8f61ff19434
Helen <kozovyakina@gmail.com>
2025-06-02 21:52:51 +0300

    html

--- main.py
+++ main.py
@@ -6468,41 +6468,57 @@ import csv
 #
 # print(res)
 
-import sqlite3
+# import sqlite3
+#
+# list_cars = [
+#     ('BMW', 54000),
+#     ('Chevrolet', 46000),
+#     ('Daewoo', 38000),
+#     ('Citroen', 29000),
+#     ('Honda', 33000)
+# ]
+#
+#
+# with sqlite3.connect("car.db") as con:
+#     cur = con.cursor()
+#     cur.execute("""
+#     CREATE TABLE IF NOT EXISTS cars(
+#         car_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         model TEXT,
+#         price INTEGER
+#     )""")
+#
+#     # for car in list_cars:
+#     #     cur.execute("INSERT INTO cars VALUES(NULL, ?, ?)", car)
+#
+#     # cur.executemany("INSERT INTO cars VALUES(NULL, ?, ?)", list_cars)
+#
+#     cur.executescript("""
+#     DELETE FROM cars WHERE model LIKE 'B%';
+#     UPDATE cars SET price = price + 100;
+#     """)
+#
+#     # cur.execute("INSERT INTO cars VALUES(1, 'Renault', 22000)")
+#     # cur.execute("INSERT INTO cars VALUES(2, 'Volvo', 29000)")
+#     # cur.execute("INSERT INTO cars VALUES(3, 'Mercedes', 57000)")
+#     # cur.execute("INSERT INTO cars VALUES(4, 'Bentley', 35000)")
+#     # cur.execute("INSERT INTO cars VALUES(5, 'Audi', 52000)")
+#
+# # con.commit()
+# # con.close()
 
-list_cars = [
-    ('BMW', 54000),
-    ('Chevrolet', 46000),
-    ('Daewoo', 38000),
-    ('Citroen', 29000),
-    ('Honda', 33000)
-]
+a = 7
 
 
-with sqlite3.connect("car.db") as con:
-    cur = con.cursor()
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS cars(
-        car_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        model TEXT,
-        price INTEGER
-    )""")
+def fun():
+    def fun1():
+        global a
+        a = 5
+        print("Inner", a)
 
-    # for car in list_cars:
-    #     cur.execute("INSERT INTO cars VALUES(NULL, ?, ?)", car)
+    fun1()
+    print("Outer", a)
 
-    # cur.executemany("INSERT INTO cars VALUES(NULL, ?, ?)", list_cars)
 
-    cur.executescript("""
-    DELETE FROM cars WHERE model LIKE 'B%';
-    UPDATE cars SET price = price + 100;
-    """)
-
-    # cur.execute("INSERT INTO cars VALUES(1, 'Renault', 22000)")
-    # cur.execute("INSERT INTO cars VALUES(2, 'Volvo', 29000)")
-    # cur.execute("INSERT INTO cars VALUES(3, 'Mercedes', 57000)")
-    # cur.execute("INSERT INTO cars VALUES(4, 'Bentley', 35000)")
-    # cur.execute("INSERT INTO cars VALUES(5, 'Audi', 52000)")
-
-# con.commit()
-# con.close()
+fun()
+print(a)
