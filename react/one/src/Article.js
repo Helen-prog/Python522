16052791a1c48db26859717cccb131bde9c04bf1
Helen <kozovyakina@gmail.com>
2025-09-17 21:37:59 +0300

    react

---
+++ react/one/src/Article.js
@@ -0,0 +1,12 @@
+import './Article.css';
+
+function Article() {
+    return (
+        <article>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi expedita commodi optio exercitationem perspiciatis perferendis blanditiis, officiis labore similique consectetur soluta dolores deserunt nihil. Ad debitis facere vel quam voluptate!</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi id a fuga molestias ullam laudantium fugit eveniet repudiandae. Illo repellendus quidem placeat ea ipsa suscipit aliquid molestias, voluptatem minus provident?</p>
+        </article>
+    )
+}
+
+export default Article;
---
+++ react/one/src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer style={{ backgroundColor: "#d2ccff", fontWeight: "bold", padding: "10px 0" }}>
+                <p>Copyright</p>
+            </footer>
+        )
+    }
+}
+
+export default Footer;
