2f69f8653671c0dda9fe40d9a315d0494c8fad09
Helen <kozovyakina@gmail.com>
2025-10-27 21:41:51 +0300

    portfolio

--- main.py
+++ main.py
@@ -6718,7 +6718,7 @@ import csv
 # if __name__ == '__main__':
 #     main()
 
-from jinja2 import Template
+# from jinja2 import Template
 
 # name = "Игорь"
 # age = 28
@@ -6797,18 +6797,44 @@ from jinja2 import Template
 # print(msg)
 
 
-html = """
-{% macro input_func(name, value='', type='text', size=20) %}
-    <input type="{{ type }}" name="{{ name }}" value="{{ value }}" size="{{ size }}">
-{% endmacro %}
+# html = """
+# {% macro input_func(name, value='', type='text', size=20) %}
+#     <input type="{{ type }}" name="{{ name }}" value="{{ value }}" size="{{ size }}">
+# {% endmacro %}
+#
+#
+# <p> {{ input_func('username') }} </p>
+# <p> {{ input_func('email') }} </p>
+# <p> {{ input_func('password', type='password') }} </p>
+# """
+#
+# tm = Template(html)
+# msg = tm.render()
+#
+# print(msg)
 
 
-<p> {{ input_func('username') }} </p>
-<p> {{ input_func('email') }} </p>
-<p> {{ input_func('password', type='password') }} </p>
-"""
+from jinja2 import Environment, FileSystemLoader
 
-tm = Template(html)
+# person = [
+#     {"name": "Алексей"},
+#     {"name": "Никита"},
+#     {"name": "Виталий"},
+# ]
+#
+# file_loader = FileSystemLoader('templates')
+# env = Environment(loader=file_loader)
+#
+# tm = env.get_template("index.html")
+# msg = tm.render(users=person, title="About Jinja")
+#
+# print(msg)
+
+file_loader = FileSystemLoader('templates')
+env = Environment(loader=file_loader)
+
+tm = env.get_template("about.html")
 msg = tm.render()
 
 print(msg)
+
