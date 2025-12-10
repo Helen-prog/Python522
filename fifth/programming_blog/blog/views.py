733f3ccb5285580999fac53269b3ad582282e034
Helen <kozovyakina@gmail.com>
2025-12-10 21:39:10 +0300

    blog

--- fifth/programming_blog/blog/views.py
+++ fifth/programming_blog/blog/views.py
@@ -3,30 +3,27 @@ from django.views.generic import ListView, DetailView, CreateView
 from .models import *
 from .forms import *
 from django.urls import reverse_lazy
+from .utils import *
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
 
-menu = [
-    {'title': 'Добавить страницу', 'url_name': 'add_page'},
-    {'title': 'Войти', 'url_name': 'index'},
-]
 
-
-class BlogHome(ListView):
+class BlogHome(DataMixin, ListView):
     model = Blog  # posts = Blog.objects.all()
     template_name = "blog/index.html"
     context_object_name = "posts"
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Главная страница'
-        context['menu'] = menu
-        context['cat_selected'] = 0
-        return context
+        c_def = self.get_user_context(title='Главная страница')
+        return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
         return Blog.objects.filter(is_published=True).select_related('cat')
 
 
-class ShowPost(DetailView):
+class ShowPost(DataMixin, DetailView):
     model = Blog  # post = Blog.objects.get(slug=post_slug)
     template_name = "blog/post.html"
     context_object_name = "post"
@@ -34,34 +31,56 @@ class ShowPost(DetailView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = context['post']
-        context['menu'] = menu
-        return context
+        c_def = self.get_user_context(title=context['post'])
+        return dict(list(context.items()) + list(c_def.items()))
 
 
-class BlogCategory(ListView):
+class BlogCategory(DataMixin, ListView):
     model = Blog  # posts = Blog.objects.all()
     template_name = "blog/index.html"
     context_object_name = "posts"
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Категория - ' + str(context['posts'][0].cat)
-        context['menu'] = menu
-        context['cat_selected'] = context['posts'][0].cat_id
-        return context
+        # context['title'] = 'Категория - ' + str(context['posts'][0].cat)
+        # context['menu'] = menu
+        # context['cat_selected'] = context['posts'][0].cat_id
+        c = Category.objects.get(slug=self.kwargs['cat_slug'])
+        c_def = self.get_user_context(title='Категория - ' + str(c.name), cat_selected=c.pk)
+        return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
         return Blog.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True).select_related('cat')
 
 
-class AddPage(CreateView):
+class AddPage(LoginRequiredMixin, DataMixin, CreateView):
     form_class = AddPostForm
     template_name = 'blog/addpage.html'
     success_url = reverse_lazy('index')
+    login_url = reverse_lazy('login')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Добавление статьи'
-        context['menu'] = menu
-        return context
+        c_def = self.get_user_context(title='Добавление статьи')
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'blog/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Регистрация')
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = AuthenticationForm
+    template_name = 'blog/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Авторизация')
+        return dict(list(context.items()) + list(c_def.items()))
