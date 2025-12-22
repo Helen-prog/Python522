262df9aaaf1d49093339dd7e2fc5fd6e811b3e2e
Helen <kozovyakina@gmail.com>
2025-12-22 21:45:29 +0300

    shop

--- sixth/shop/products/urls.py
+++ sixth/shop/products/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('products/', views.products, name='products'),
+    path('products/<int:category_id>/', views.products, name='category'),
+    path('product/<int:pk>/', views.product, name='product'),
 ]
